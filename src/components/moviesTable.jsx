9a05c0cb3b477fd3ba6fc1ddfa04b6ad4402b539
Ankit Singh Chauhan <ankitt335407@gmail.com>
2023-03-06 21:53:14 +0530

    new file:   src/components/common/tableHeader.jsx modified:   src/components/moviesTable.jsx

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -2,16 +2,6 @@ import React, { Component } from "react";
 import Like from "./common/like";
 
 class MoviesTable extends Component {
-  raiseSort = (path) => {
-    const sortColumn = {...this.props.sortColumn};
-    if(sortColumn.path === path){
-      sortColumn.order = (sortColumn.order === "asc") ? "desc" : "asc";
-    }else {
-      sortColumn.path = path;
-      sortColumn.order = "asc";
-    }
-    this.props.onSort(sortColumn);
-  };
   render() {
     const { movies, onDelete, onLike, onSort } = this.props;
     return (
@@ -22,7 +12,7 @@ class MoviesTable extends Component {
             <th onClick={() => this.raiseSort("genre.name")}>Genre</th>
             <th onClick={() => this.raiseSort("numberInStock")}>Stock</th>
             <th onClick={() => this.raiseSort("dailyRentalRate")}>Rate</th>
-            <th />
+            <th /> 
           </tr>
         </thead>
         <tbody>
